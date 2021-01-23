f0cd9a7c8f247abad918195184f6d079df13e69c
PARK-JONGSEOK <pjongsuk1@gmail.com>
2021-01-23 13:16:25 +0900

    Employee 추가

---
+++ springboot-backend/src/main/java/com/example/springboot/model/Employee.java
@@ -0,0 +1,35 @@
+package com.example.springboot.model;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "employyes")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String fistName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    
+    @Column(name = "email_id")
+    private String emailId;
+
+    public Employee(String fistName, String lastName, String emailId) {
+        this.fistName = fistName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+}
