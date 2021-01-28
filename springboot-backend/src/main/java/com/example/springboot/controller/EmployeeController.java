76e64e1a239e7d6ff87a3c48effbae8a38bb57da
PARK-JONGSEOK <pjongsuk1@gmail.com>
2021-01-28 11:07:13 +0900

    Employee 코드 수정 및 Controller 추가

--- springboot-backend/src/main/java/com/example/springboot/controller/EmployeeController.java
+++ springboot-backend/src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -1,14 +1,17 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.exception.ResourceNotFoundException;
 import com.example.springboot.model.Employee;
 import com.example.springboot.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
@@ -20,5 +23,40 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    // Get Employee By Id
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id : " + id));
+        return ResponseEntity.ok(employee);
+    }
+
+    // Create Employee
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    // Update Employee
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id : " + id));
+        employee.changeEmployee(employeeDetails.getFirstName(), employeeDetails.getLastName(), employeeDetails.getEmailId());
+
+        Employee updateEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updateEmployee);
+    }
+
+    // Delete Employee
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id : " + id));
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("Deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
     }
 }
--- springboot-backend/src/main/java/com/example/springboot/model/Employee.java
+++ springboot-backend/src/main/java/com/example/springboot/model/Employee.java
@@ -3,12 +3,10 @@ package com.example.springboot.model;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "employees")
@@ -19,16 +17,22 @@ public class Employee {
     private Long id;
 
     @Column(name = "first_name")
-    private String fistName;
+    private String firstName;
 
     @Column(name = "last_name")
     private String lastName;
-    
+
     @Column(name = "email_id")
     private String emailId;
 
-    public Employee(String fistName, String lastName, String emailId) {
-        this.fistName = fistName;
+    public Employee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public void changeEmployee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
         this.lastName = lastName;
         this.emailId = emailId;
     }
